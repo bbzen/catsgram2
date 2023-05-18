0e6ed585e1bbe8d4f9e0f65ef4f836b06566ef16
GagarinSN <gagarin.sn@yandex.ru>
2023-05-18 22:08:34 +0500

    feat:do user controller

--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -1,15 +1,13 @@
 package ru.yandex.practicum.catsgram.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.catsgram.exception.InvalidEmailException;
 import ru.yandex.practicum.catsgram.exception.UserAlreadyExistException;
 import ru.yandex.practicum.catsgram.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -21,17 +19,29 @@ public class UserController {
     }
 
     @PostMapping(value = "/users")
-    public void create(@RequestBody User user) {
-        boolean hasNoEmail = user.getEmail().isBlank() || user.getEmail().isEmpty() || user.getEmail() == null;
-        if (hasNoEmail) {
-            throw new InvalidEmailException("Отсутствует адрес электронной почты.");
-        }
-
-        boolean checkEmail = users.stream().anyMatch(a -> a.getEmail().equalsIgnoreCase(user.getEmail()));
-        if (!checkEmail) {
-            users.add(user);
-        } else {
+    public User create(@RequestBody User user) {
+        isNoEmail(user);
+        boolean isEmailExists = users.stream().anyMatch(a -> a.getEmail().equalsIgnoreCase(user.getEmail()));
+        if (isEmailExists) {
             throw new UserAlreadyExistException("Пользователь с указанным адресом электронной почты уже существует.");
+        }
+        users.add(user);
+        return user;
+    }
+
+    @PutMapping("/users")
+    public User update(@RequestBody User user) {
+        isNoEmail(user);
+
+        Optional<User> optUser = users.stream().filter(a -> a.getEmail().equalsIgnoreCase(user.getEmail())).findAny();
+        optUser.ifPresent(users::remove);
+        users.add(user);
+        return user;
+    }
+
+    private void isNoEmail(User user) {
+        if (user.getEmail().isBlank() || user.getEmail().isEmpty() || user.getEmail() == null) {
+            throw new InvalidEmailException("Отсутствует адрес электронной почты.");
         }
     }
 }
